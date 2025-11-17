cab0ed81d77986a9d82617ce58e1f93d7ed89916
Anne Lüken <anne.lueken@ahoii.net>
2025-11-17 15:09:26 +0100

    gave artwork preview a use

--- components/ArtworkList.js
+++ components/ArtworkList.js
@@ -1,12 +1,11 @@
 import Image from "next/image";
+import ArtworkPreview from "./Artworkpreview";
 
 export default function ArtworkList({ artist, title, artwork }) {
   return (
     <>
       <li>
-        <p>{artist}</p>
-        <p>{title}</p>
-        <Image src={artwork} alt={title} width={140} height={230}></Image>
+        <ArtworkPreview></ArtworkPreview>
       </li>
     </>
   );
